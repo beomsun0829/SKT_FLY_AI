a36ea687d15d6a299a8d8bf58abb6464e0be389c
beomsun0829 <ghsqjatns@gmail.com>
2024-02-02 17:56:25 +0900

    ch

--- 20240131/GenAI/chatgpt-00.py
+++ 20240131/GenAI/chatgpt-00.py
@@ -1,4 +1,5 @@
 import openai
+import streamlit as st
 
 import os
 import dotenv
@@ -14,14 +15,28 @@ openai.azure_endpoint = OPENAI_API_ENDPOINT
 openai.api_type = OPENAI_API_TYPE
 openai.api_version = OPENAI_API_VERSION
 
-query = "Why Korean people work so hard?"
+st.header("AI Poem Generator", divider="rainbow")
 
-result = openai.chat.completions.create(
-    model="dev-gpt-35-turbo",
-    messages=[
-        {"role" : "system", "content" : "you are a helpful assistant"},
-        {"role" : "user", "content" : query},
-    ]
-)
+name = st.text_input("Input Name : ")
+if name:
+    st.write(f"Hello {name}! How can I help you today?")
 
-print(result.choices[0].message.content)
+subject = st.text_input("Input Subject : ")
+content = st.text_input("Input Content : ")
+
+button_result = st.button("Run")
+if button_result:
+    with st.spinner("Generating poem..."):
+        result = openai.chat.completions.create(
+            model="dev-gpt-35-turbo",
+            messages=[
+                {"role" : "system", "content" : "you are a poem generator."},
+                {"role" : "user", "content" : "Name of the writer is " + name},
+                {"role" : "user", "content" : "Subject of poem is " + subject},
+                {"role" : "user", "content" : "Content of poem is " + content},
+                {"role" : "user", "content" : "Based on the above information, please generate a poem."}
+            ]
+        )
+    
+    st.write(result.choices[0].message.content)
+    st.success("Poem generated successfully!")
